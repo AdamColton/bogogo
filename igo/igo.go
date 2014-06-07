56ecfb6959b24d7928212468637009554604cff3
Adam Colton <adamcolton@gmail.com>
2014-06-07 11:24:06 -0400

    Check for win working

--- igo/igo.go
+++ igo/igo.go
@@ -9,44 +9,122 @@ type Game struct {
 type Coord [2]int
 
 type group struct {
-  player int
   Coords []Coord
+  eye bool
 }
 
-func (self *Game) Init()(){
+type chain map[Coord]bool
+
+func (self *Game) Board(x,y int)(int) { return self.board[x][y] }
+func (self *group) Eye()(bool) { return self.eye }
+
+func (self *Game) Init(sides int)(){
   self.turn = 1
-}
-
-func (self *Game) Move(x, y int)(){
-  if (self.board[x][y] == 0){
-    self.board[x][y] = self.turn
-    self.turn = 3 - self.turn
+  self.sides = sides
+  if (self.sides > 2){
+    self.sides = 2
   }
 }
 
-func (self *Game) GetGroup(x, y, player int)(group){
-  g := group{}
-  g.player = player
-  processed := make(map[Coord]bool)
-  q := make([]Coord, 0)
-  i := Coord{x,y}
+func (self *Game) Move(x, y int)(bool, []group, chain){
+  win := false
+  gs := make([]group,0,0)
+  chn := chain{}
+  if (self.board[x][y] == 0){
+    self.board[x][y] = self.turn
+    win, gs, chn = self.CheckForWin(x,y)
+    self.turn = 3 - self.turn
+  }
+  return win, gs, chn
+}
+
+func (self *Game) GetChain(x,y int)(chain){
+  p := self.board[x][y]
+  proc := make(map[Coord]bool)
+  q := make([]Coord, 0, 100)
+  c := Coord{x,y}
+  chn := chain{} //? make(chain)
   dirs := [...]Coord{ {-1,0}, {1,0}, {0, -1}, {0, 1}}
 
-  q = append(q, i)
-  processed[i] = true
+  q = append(q,c)
+  proc[c] = true
+
+  for len(q) > 0 {
+    c, q = q[len(q)-1], q[:len(q)-1]
+    if (c[0] >= 0 && c[0] < 10 && c[1] >= 0 && c[1] < 10 && self.board[c[0]][c[1]] == p){
+      chn[c] = true
+      for _, dir := range dirs {
+        t := Coord{dir[0]+c[0], dir[1]+c[1]}
+        if (!proc[t]) {
+          proc[t] = true
+          q = append(q,t)
+        }
+      }
+    }
+  }
+  return chn
+}
+
+func (self *Game) GetGroup(x, y int, chn chain)(group){
+  g := group{}
+  g.eye = true
+  proc := make(map[Coord]bool)
+  q := make([]Coord, 0, 100)
+  c := Coord{x,y}
+  dirs := [...]Coord{ {-1,0}, {1,0}, {0, -1}, {0, 1}}
+
+  q = append(q, c)
+  proc[c] = true
 
   for(len(q) > 0){
-    i, q = q[len(q)-1], q[:len(q)-1]
-    if (i[0] >= 0 && i[0] < 10 && i[1] >= 0 && i[1] < 10 && self.board[i[0]][i[1]] != player){
-      g.Coords = append(g.Coords, i)
+    c, q = q[len(q)-1], q[:len(q)-1]
+    if (c[0] >= 0 && c[0] < 10 && c[1] >= 0 && c[1] < 10 && !chn[c]){
+      g.Coords = append(g.Coords, c)
       for _, dir := range dirs {
-        t := Coord{dir[0]+i[0], dir[1]+i[1]}
-        if (!processed[t]){
-          processed[t] = true
+        t := Coord{dir[0]+c[0], dir[1]+c[1]}
+        if (!proc[t]){
+          proc[t] = true
           q = append(q, t)
+          if( g.eye && 
+              ( t[0] == 10 ||
+                t[1] == 10 ||
+                (self.sides < 2 && t[0] < 0) ||
+                (self.sides == 0 && t[1] < 0))) {
+            g.eye = false
+          }
         }
       }
     }
   }
   return g
+}
+
+func (self *Game) GetGroups(chn chain)([]group) {
+  gs := make([]group, 0)
+  proc := make(map[Coord]bool)
+  for x := 0 ; x<10 ; x++ {
+    for y := 0 ; y<10 ; y++ {
+      c := Coord{x,y}
+      if (!chn[c] && !proc[c]){
+        g := self.GetGroup(x, y, chn)
+        gs = append(gs, g)
+        for _, c := range g.Coords {
+          proc[c] = true
+        }
+      }
+    }
+  }
+  return gs
+}
+
+func (self *Game) CheckForWin(x,y int)(bool, []group, chain) {
+  chn := self.GetChain(x,y)
+  gs := self.GetGroups(chn)
+  c := 0
+  for _, g := range gs {
+    if (g.eye){
+      c++
+    }
+  }
+  return c >= 2, gs, chn
 }
--- igo/igo_test.go
+++ igo/igo_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestInit(t *testing.T){
   game := Game{}
-  game.Init()
+  game.Init(2)
 
   if (game.board[0][0] != 0){
     t.Error("Expected board to initilize to 0")
@@ -15,6 +15,24 @@ func TestInit(t *testing.T){
 
   if (game.turn != 1){
     t.Error("Expected turn to initilize to 1")
+  }
+}
+
+func TestMove(t *testing.T) {
+  game := Game{}
+  game.Init(2)
+  game.Move(0,0)
+  game.Move(0,1)
+  game.Move(0,2)
+
+  if (game.Board(0,0) != 1){
+    t.Error( "expected 0,0 to be 1, got "+strconv.Itoa(game.Board(0,0)) )
+  }
+  if (game.Board(0,1) != 2){
+    t.Error( "expected 0,1 to be 2, got "+strconv.Itoa(game.Board(0,1)) )
+  }
+  if (game.Board(0,2) != 1){
+    t.Error( "expected 0,2 to be 1, got "+strconv.Itoa(game.Board(0,2)) )
   }
 }
 
@@ -30,11 +48,57 @@ func TestCoordQueue(t *testing.T) {
   }
 }
 
-func TestGetGroupEmptyBoard(t *testing.T) {
-  g := Game{}
-  g.Init()
-  grp := g.GetGroup(0,0,1)
-  if (len(grp.Coords) != 100){
-    t.Error("expected 100, got " + strconv.Itoa(len(grp.Coords)))
+func TestOneEye(t *testing.T) {
+  game := Game{}
+  game.Init(2)
+
+  game.Move(0,1)
+  game.Move(0,2)
+  game.Move(1,0)
+  game.Move(1,2)
+  win, gs, chn := game.Move(1,1)
+
+  if (len(chn) != 3){
+    t.Error("Expected len 3, got ", len(chn))
+  }
+
+  if (win) {
+    t.Error("did not expect a win")
+  }
+
+  if (len(gs) != 2){
+    t.Error("Expected 2 groups, got ", len(gs))
+  }
+
+  c := 0
+  for _, g := range gs {
+    if (g.Eye()){
+      c++
+    }
+  }
+  if (c != 1){
+    t.Error("Expected one eye, got ", c)
+  }
+}
+
+func TestWin(t *testing.T) {
+  game := Game{}
+  game.Init(2)
+
+  game.Move(1,0)
+  game.Move(2,0)
+  game.Move(0,1)
+  game.Move(2,1)
+  game.Move(1,1)
+  game.Move(2,2)
+  game.Move(1,2)
+  game.Move(2,3)
+  game.Move(1,3)
+  game.Move(2,4)
+
+  win, _, _ := game.Move(0,3)
+
+  if (!win) {
+    t.Error("Expected a win")
   }
 }
